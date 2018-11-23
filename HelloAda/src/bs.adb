9db1851d67d0914fe4c0de10c99278807a7d864a
chongshen <shenchong123@yahoo.com>
2018-11-23 12:45:03 +0100

    add all

---
+++ HelloAda/src/bs.adb
@@ -0,0 +1,41 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure Bs is
+   V:Integer := 1;
+   U:Integer := 2;
+
+begin
+   PutVU1:
+   begin
+      Put("V is:");
+      Put(V);
+      New_Line;
+
+      Put("U is:");
+      Put(U);
+      New_Line;
+   end PutVU1;
+
+   Swap:
+   declare
+      Temp :Integer;
+   begin
+      Temp := V;
+      V:=U;
+      U:=Temp;
+   end Swap;
+
+   Put_Line ("After swap");
+
+   PutVU2:
+   begin
+      Put("V is:");
+      Put(V);
+      New_Line;
+
+      Put("U is:");
+      Put(U);
+      New_Line;
+   end PutVU2;
+end Bs;
---
+++ HelloAda/src/helloworld.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Helloworld is
+   Str:String := "Hello World!";
+
+begin
+   Ada.Text_IO.Put(Str);
+   Ada.Text_IO.New_Line;
+end Helloworld;
